a10f26cfb2c2c644e41ef7c84ff130c113e308de
tkdhun <gnsdkaos99@naver.com>
2022-12-08 17:13:09 +0900

    Create boj-14645.cpp

---
+++ Project69/Project69/boj-14645.cpp
@@ -0,0 +1,18 @@
+//boj-14645
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+	int n = 0, k = 0, a = 0, b = 0;
+
+	scanf("%d %d", &n, &k);
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d %d", &a, &b);
+	}
+
+	printf("ºñ¿ÍÀÌ\n");
+
+	return 0;
+}
